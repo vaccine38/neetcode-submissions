b3f42fe42df2836ecc0fadea3921a3243332d26c
vaccine38 <39764911+vaccine38@users.noreply.github.com>
2026-09-14 17:07:00 +0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.go
@@ -0,0 +1,21 @@
+func groupAnagrams(strs []string) [][]string {
+
+	strMap := make(map[string][]string)
+	for _, str := range strs {
+		chars := make([]byte, 26)
+		for j := range len(str) {
+			chars[str[j] - 'a'] = chars[str[j] - 'a'] + 1
+		}
+		var key strings.Builder
+		for _, v := range chars {
+			key.WriteString(fmt.Sprintf("%v,", v))
+		}
+		strMap[key.String()] = append(strMap[key.String()], str)
+	}
+
+	ans := make([][]string, 0)
+	for _, v := range strMap {
+		ans = append(ans, v)
+	}
+	return ans
+}
